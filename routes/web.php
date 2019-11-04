3eec04bab2a3de8ab9fffeb7549809618b53f78a
Haris Putratama <lastblezebub@gmail.com>
2019-11-04 15:42:23 +0700

    1st up

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $nama = 'Haris';
+    return view('index', ['nama' => $nama]);
+});
+
+Route::get('/order', function () {
+    $nama = 'Haris';
+    return view('order',  ['nama' => $nama]);
+});
+
+Route::get('/karyawan', function () {
+    $nama = 'Haris';
+    return view('karyawan',  ['nama' => $nama]);
+});
+
+Route::get('/keuangan', function () {
+    $nama = 'Haris';
+    return view('keuangan',  ['nama' => $nama]);
+});
+
+Route::get('/akta', function () {
+    $nama = 'Haris';
+    return view('akta',  ['nama' => $nama]);
+});
+
+Route::get('/user', function () {
+    $nama = 'Haris';
+    return view('user',  ['nama' => $nama]);
+});
+
