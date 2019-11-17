1233f221055bd59bbc896a4210c3c24ffa665b32
adorabable <whatthefapmc@gmail.com>
2019-11-17 20:53:15 +0700

    login 70%

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,71 @@
 @extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Login - Anota</title>
+</head>
+<link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="/css/signin.css" rel="stylesheet">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  <body class="text-center" background="/assets/bg/login/background.png">
+    <div class="col-md-12" style="background:url(/assets/bg/login/Exclusion.png); height: 700px; background-size: 700px; background-repeat: no-repeat; background-position: center;">
+      <form class="form-signin" action="{{ route('login') }}" method="POST">
+      @csrf
+        <div class="h1 font-weight-normal title">Anota
+          <div class="h5 mb-3 font-weight-normal subtitle">Asisten Notaris Anda</div>
         </div>
+
+        <div class="align-left"><img class="logintext" src="/assets/bg/login/login.png"></div>
+        
+        <div class="align-left mb-4 font-weight-normal"style="font-size: 15px;">Masukkan Identitas Anda Untuk Melanjutkan</div>
+        
+        <label for="inputEmail" class="sr-only">Username</label>
+        <div class="align-left"><img src="/assets/bg/login/username.png "style="height=45px;width:45px">
+          <input type="email" name="email" id="inputEmail" class="form-control custom-form @error('email') is-invalid @enderror" placeholder="Email" required autofocus>
+        </div>
+        
+        <label for="inputPassword" class="sr-only">Password</label>
+        <div class="align-left"><img src="/assets/bg/login/password.png "style="height=45px;width:45px">
+          <input type="password" name ="password" id="inputPassword" class="form-control custom-form @error('password') is-invalid @enderror" placeholder="Password" required>
+        </div>
+        
+        <div class="checkbox mb-3">
+            <label style="margin-right: 170px;">
+            <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            <div class="h8"style="margin-right: 70px;">Lupa Password ?   <a href="url">Hubungi Kami</a></div> 
+        </div>
+        
+        <button class="btn btn-2" type="submit" name="submit">Sign in </button>
+        <p class="mt-5 mb-3 text-muted">&copy; Anota 2019</p>
+      </form>
     </div>
-</div>
-@endsection
+  </body>
+  </html>
