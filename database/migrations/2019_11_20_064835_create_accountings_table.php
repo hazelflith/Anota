ad598d020a628009bb851e3b09520f94264e8714
cryssapiter <cryssapiter13@gmail.com>
2019-11-20 14:05:53 +0700

    Database Accounting dan Order

---
+++ database/migrations/2019_11_20_064835_create_accountings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accountings', function (Blueprint $table) {
+            $table->Increments('idAccounting');
+            $table->string('idOrder');
+            $table->foreign('idOrder')->references('idOrder')->on('orders');
+            $table->integer('priceOrder');
+            $table->integer('biayaMasuk');
+            $table->integer('biayaSisa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accountings');
+    }
+}
